09df0a44fd56bbae3b0447c759e11fa2cb9eb2bf
Santhosh M <msanthosh9943@gmail.com>
2023-06-09 16:08:47 +0530

    project models

---
+++ src/main/java/com/santhosh/projects/Spring_CURD/entity/User.java
@@ -0,0 +1,29 @@
+package com.santhosh.projects.Spring_CURD.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+}
